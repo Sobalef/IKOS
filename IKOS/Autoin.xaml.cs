6ba215e55e1cba932fb55b1d23eb88525dd64aa1
ОВП <@DESKTOP-BC345LG>
2022-06-09 03:52:00 +0300

    Добавьте файлы проекта.

---
+++ IKOS/Autoin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IKOS
+{
+    /// <summary>
+    /// Логика взаимодействия для Autoin.xaml
+    /// </summary>
+    public partial class Autoin : Window
+    {
+        public Autoin()
+        {
+            InitializeComponent();
+        }
+    }
+}
